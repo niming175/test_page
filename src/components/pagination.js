ac6267791be330135e15319417ed3f07b0707d6c
niming175 <niming175@qq.com>
2019-11-11 11:30:21 +0800

    增加分页， 添加关于我们

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -3,27 +3,61 @@
  */
 export const pagination = (page = 1, total=0) => {
 
+  // return `
+  //   <ul class="pagination">
+  //     <li class="disabled">
+  //       <a href="#" aria-label="Previous">
+  //         <span>«</span>
+  //       </a>
+  //     </li>
+  //     <li class="active">
+  //       <a href="#">
+  //         1 <span class="sr-only">(current)</span>
+  //       </a>
+  //     </li>
+  //     <li><a href="#">2</a></li>
+  //     <li><a href="#">3</a></li>
+  //     <li><a href="#">4</a></li>
+  //     <li><a href="#">5</a></li>
+  //     <li>
+  //       <a href="#" aria-label="Next">
+  //         <span aria-hidden="true">»</span>
+  //       </a>
+  //     </li>
+  //   </ul>
+  // `
+
+  var firstHtml = `
+    <li class="${page === 1 && 'disabled'}">
+      <a href="#" aria-label="Previous">
+        <span>«</span>
+      </a>
+    </li>
+  `
+  var lastHtml = `
+    <li class="${page === 1 && 'disabled'}">
+      <a href="#" aria-label="Next">
+        <span aria-hidden="true">»</span>
+      </a>
+    </li>
+  `
+  var node = '';
+  for (var i=1; i < (total/10) + 1; i ++ ) {
+    node += `
+      <li class="${page === i && 'active'}">
+        <a page='${i}'>
+          ${i}
+          ${(page === i) ? `<span class="sr-only">(current)</span>` : ''}
+        </a>
+      </li>
+    `
+  }
+
   return `
     <ul class="pagination">
-      <li class="disabled">
-        <a href="#" aria-label="Previous">
-          <span>«</span>
-        </a>
-      </li>
-      <li class="active">
-        <a href="#">
-          1 <span class="sr-only">(current)</span>
-        </a>
-      </li>
-      <li><a href="#">2</a></li>
-      <li><a href="#">3</a></li>
-      <li><a href="#">4</a></li>
-      <li><a href="#">5</a></li>
-      <li>
-        <a href="#" aria-label="Next">
-          <span aria-hidden="true">»</span>
-        </a>
-      </li>
+      ${firstHtml}
+      ${node}
+      ${lastHtml}
     </ul>
   `
-} 
+}
